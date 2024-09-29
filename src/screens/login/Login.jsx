f30e83424828deb883fc9c101b03ccbdba7cf974
hasanraza38 <92hasanraza689@gmail.com>
2024-09-29 18:01:32 +0500

    updates

--- src/screens/login/Login.jsx
+++ src/screens/login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from '../../components/navbar/Nav'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Login() {
 const navigate = useNavigate()
@@ -10,8 +10,8 @@ const navigate = useNavigate()
 <Nav/>
 
 <div className='flex justify-center items-center h-[90vh]'> 
-  <div className='border  border-zinc-400 p-11 rounded-2xl shadow-2xl shadow-[#43f7e2]'>
-  <h1 className='text-center font-bold text-white text-3xl'> Register</h1>
+  <div className='border  border-zinc-400 p-11 rounded-2xl shadow-lg shadow-[#43f7e2]'>
+  <h1 className='text-center font-bold text-white text-3xl'>Login</h1>
   <form className='flex justify-center flex-col items-center mt-8' >
   <div className="mb-4 w-80">
     <input
@@ -29,10 +29,7 @@ const navigate = useNavigate()
       placeholder="Enter your password"
     />
   </div>
-  <div className='w-62 flex justify-center items-center '>
-
-  <input type="file" className="file-input bg-[#00D9C0] text-black file-input-bordered file-input-accent w-full max-w-xs" />
-  </div>
+  
 
   <div className=" mt-2 flex justify-center">
     <button
@@ -43,7 +40,7 @@ const navigate = useNavigate()
     </button>
   </div>
   <div className="flex justify-center mt-2">
-    <h1 className='text-white text-md'>Not a user ? <span onClick={navigate('/register')} className='text-[#00D9C0]'>Register</span></h1>
+    <h1 className='text-white text-md'>Not a user ? <span className='text-[#00D9C0] cursor-pointer'><Link to='/register'>Register</Link></span></h1>
   </div>
 </form>
   </div>
--- src/screens/register/Register.jsx
+++ src/screens/register/Register.jsx
@@ -1,9 +1,114 @@
-import React from 'react'
+import React, { useRef } from "react";
+import Nav from "../../components/navbar/Nav";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  // const onSubmit = (data) => console.log(data);
+
+  const registerUserFromFirebase = async (data) => {
+    console.log(data);
+    
+    data.preventDefault()
+    console.log(data.email)
+    console.log(data.password)
+    console.log(data.profileImage[0])
+
+    const userProfileImageUrl = await uploadImage(data.profileImage[0], data.email)
+
+    try {
+      const userData = await signUpUser({
+        email: data.email,
+        password: data.password,
+        profileImage: userProfileImageUrl
+      })
+      console.log(userData);
+
+    } catch (error) {
+      console.error(error);
+
+    }
+
+  }
+
+
   return (
-    <div>register</div>
-  )
+    <>
+      <Nav />
+
+      <div className="flex justify-center items-center h-[90vh]">
+        <div className="border  border-zinc-400 p-11 rounded-2xl shadow-lg shadow-[#43f7e2]">
+          <h1 className="text-center font-bold text-white text-3xl">
+            {" "}
+            Register
+          </h1>
+          <form
+            onSubmit={handleSubmit(registerUserFromFirebase)}
+            className="flex justify-center flex-col items-center mt-8"
+          >
+            <div className="mb-4 w-80">
+              <input
+                type="email"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                placeholder="Enter your email"
+                {...register("email", { required: true })}
+              />
+              <br />
+              {errors.email && (
+                <span className="text-red-500">This field is required</span>
+              )}
+            </div>
+            <div className="mb-6 w-80">
+              <input
+                type="password"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                placeholder="Enter your password"
+                {...register("password", { required: true, minLength: 9 })}
+              />
+              <br />
+              {errors.password && (
+                <span className="text-red-500">This field is required</span>
+              )}
+            </div>
+            <div className="w-62 flex justify-center items-center ">
+              <input
+                type="file"
+                className="file-input bg-[#00D9C0] text-black file-input-bordered file-input-accent w-full max-w-xs"
+                {...register("profileImage", { required: true })}
+
+              />
+              <br />
+              {errors.password && (
+                <span className="text-red-500">This field is required</span>
+              )}
+
+            </div>
+
+            <div className=" mt-2 flex justify-center">
+              <button className="btn btn-sm text-black bg-[#00D9C0] border-none px-5 py-1 hover:bg-[#00B5A2]">
+                Register
+              </button>
+            </div>
+            <div className="flex justify-center mt-2">
+              <h1 className="text-white text-md">
+                Already a user ?{" "}
+                <span className="text-[#00D9C0] cursor-pointer">
+                  <Link to="/login">Login</Link>
+                </span>
+              </h1>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Register;
